f9d047800e8da04a29a939b37685aaa73aa30ea2
pnb-rae <ryanwaweru475@gmail.com>
2025-07-12 16:57:51 +0300

    new update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
-import WelcomeMessage from './components/WelcomeMessage';
+
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
 
 
