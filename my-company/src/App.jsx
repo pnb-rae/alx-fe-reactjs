0893b5bfbcc95136e8c8a211c35ae4acafb6c249
pnb-rae <ryanwaweru475@gmail.com>
2025-07-31 21:40:13 +0300

    new react

---
+++ my-company/src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <div style={{ fontFamily: 'Arial, sans-serif' }}>
+        {/* Navigation Bar */}
+        <Navbar />
+
+        {/* Page Content */}
+        <div style={{ padding: '20px' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
