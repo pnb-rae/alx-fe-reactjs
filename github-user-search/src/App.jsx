7da293001cba56bb8db3e3103f8aea239c8053c0
pnb-rae <ryanwaweru475@gmail.com>
2025-08-03 20:52:37 +0300

    new updates

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1 style={{ textAlign: 'center' }}>GitHub User Search</h1>
+      <p style={{ textAlign: 'center' }}>Start building your search feature...</p>
+    </div>
+  );
+}
+
+export default App;
+
