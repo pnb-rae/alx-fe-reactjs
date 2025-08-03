867113af223df4085b8c691d0ea341b9978219fd
pnb-rae <ryanwaweru475@gmail.com>
2025-08-03 20:30:26 +0300

    new fix

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,35 +1,39 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetailsWrapper from './components/RecipeDetailsWrapper';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <header>
-        <h1>Recipe Sharing App</h1>
-        <nav style={{ marginBottom: '20px' }}>
-          <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-        </nav>
-      </header>
+    <BrowserRouter>
+      <div style={{ padding: '20px' }}>
+        <header>
+          <h1>Recipe Sharing App</h1>
+          <nav style={{ marginBottom: '20px' }}>
+            <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+          </nav>
+        </header>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
 
 
 
